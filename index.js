6c4cea2520815eac949b0e7ef1477aee599f9256
Philippe Coval <p.coval@samsung.com>
2019-01-28 23:31:24 +0100

    lib: Add index.js for IoT.js
    IoT.js is not parsing package.json yet
    
    Change-Id: I4d57197ede3f3787383477805bf4070b056d3cd4
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+/* -*- mode: js; js-indent-level:2;  -*-
+ *  SPDX-License-Identifier: MIT
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the MIT Licence
+ * If a copy of the MIT was not distributed with this file
+ * You can obtain one at:
+ * https://spdx.org/licenses/MIT.html
+ */
+
+module.exports = require('./lib/express');
