a6ca06b08c01a1859ecb15cf7d330b9e907ef7c4
Philippe Coval <p.coval@samsung.com>
2019-02-22 19:36:17 +0100

    example: Add http client as alternative to curl
    Not really useful
    
    Change-Id: Ia00c6964a0e6be412cc4a48e03d592da1788c1ad
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ example/client.js
@@ -0,0 +1,64 @@
+/* -*- mode: js; js-indent-level:2;  -*-
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the MIT Licence
+ * If a copy of the MIT was not distributed with this file
+ * You can obtain one at:
+ * https://spdx.org/licenses/MIT.html
+ */
+
+var console = require('console');
+var http = require('http');
+
+
+var options = {
+  hostname: 'localhost',
+  port: 8888,
+  path: '/'
+};
+
+
+function receive(incoming, callback) {
+  var data = '';
+  incoming.on('data', function (chunk) {
+    data += chunk;
+  });
+
+  incoming.on('end', function () {
+    if (callback) {
+      return callback(data);
+    }
+
+    return null;
+  });
+}
+
+// Main
+if (module.parent === null) {
+  if (process.argv[2]) {
+    options.port = Number(process.argv[2]);
+  }
+  if (process.argv[3]) {
+    options.hostname = process.argv[3];
+  }
+  if (process.argv[4]) {
+    options.path = process.argv[4];
+  }
+  // Workaround bug
+  if (!options.headers) {
+    options.headers = {};
+  }
+  if (!options.headers.host) {
+    options.headers.host = options.hostname;
+  }
+
+  http.request(options, function (res) {
+    receive(res, function (data) {
+      console.log(data);
+      var object = JSON.parse(data);
+      console.log(JSON.stringify(object));
+    });
+  }).end();
+}
