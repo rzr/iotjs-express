977f9aeaf8d5a8471395880cbf7b492538f88550
Philippe Coval <p.coval@samsung.com>
2019-10-15 17:40:28 +0200

    openfaas: Support OpenFaaS Cloud
    Only client is used as function (using node runtime):
    
    Usage:
    
    ```
    echo '{ "hostname": "ifconfig.io", "port": 80 , "path":"/all.json" }' \
      | curl -H "Content-Type: application/json" -X POST -d @- \
      https://rzr.o6s.io/iotjs-express
    
    ```
    
    Relate-to: github.com/openfaas/openfaas-cloud/pull/523
    Change-Id: I49ca87f508a52e922414f86cbce2e40487fb023a
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ extra/tools/openfaas/handler.js
@@ -0,0 +1,17 @@
+/* -*- mode: js; js-indent-level:2;  -*-
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2019-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the MIT Licence
+ * If a copy of the MIT was not distributed with this file
+ * You can obtain one at:
+ * https://spdx.org/licenses/MIT.html
+ *
+ */
+"use strict"
+var ClientRequest = require('iotjs-express/example/client');
+
+module.exports = (context, callback) => {
+  ClientRequest(JSON.parse(context || {}), callback);
+}
